562bdf41df8ff2f13fda3457d20ce33c27624dd9
Shannon Silva <Shannon-Silva>
2018-06-14 22:45:42 +0530

    delete spring project

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -Dserver.port=$PORT -jar target/user-management-service-0.1.0.jar
--- src/main/java/com/koombiyo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.koombiyo.repository;
-
-import com.koombiyo.models.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface UserRepository extends MongoRepository<User, String> {
-    public User findByName(String name);
-}
