7c9ae1533d4fcfba1e440c3bf5b2a133b6bd6367
koombiyo1234 <39003317+koombiyo1234@users.noreply.github.com>
2018-06-15 16:15:34 +0530

    Heroku port config

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const Mongoose = require("mongoose");
 
 var jwt    = require('jsonwebtoken');
 
-
+const PORT = process.env.PORT || 8083;
 const app = Express();
 app.use(Cors());
 
@@ -22,7 +22,7 @@ Mongoose.connect('mongodb://abc123:abc123@ds147450.mlab.com:47450/koombiyo-af', 
 });
 
 
-app.listen(8083, 'localhost', (err) => {
+app.listen(PORT, 'localhost', (err) => {
     if(err) {
         console.log(err);
         process.exit(-1);
